2d6a6f71984a6e30813f23e1077bc2a9963a0fa5
IshniyazovFail <filimen@mail.ru>
2022-04-12 13:46:20 +0300

    pop and push

---
+++ Basic js/push.js
@@ -0,0 +1,9 @@
+// Push ["dog", 3] onto the end of the myArray variable.
+const myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog", 3]);
+
+// Manipulate Arrays With pop()
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
