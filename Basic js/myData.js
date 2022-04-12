6ee6d4fd74d3e86b31f8beabb1ef1f43937cc626
IshniyazovFail <filimen@mail.ru>
2022-04-12 13:38:14 +0300

    Access Multi-Dimensional Arrays With Indexes

--- Basic js/myData.js
+++ Basic js/myData.js
@@ -2,3 +2,14 @@
 
 const myArray = [50, 60, 70];
 const myData = myArray[0];
+
+//Access Multi-Dimensional Arrays With Indexes
+// Note: There shouldn't be any spaces between the array name and the square brackets, like array [0][0] and even this array [0] [0] is not allowed. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+const myArray = [
+   [1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9],
+   [[10, 11, 12], 13, 14],
+ ];
+
+ const myData = myArray[2][1];
