cd9e166d598ec52f4947af262f049444f141bc84
IshniyazovFail <filimen@mail.ru>
2022-04-12 13:25:22 +0300

    nested array

--- Basic js/myArray.js
+++ Basic js/myArray.js
@@ -1,4 +1,4 @@
 //Modify the new array myArray so that it contains both a string and a number (in that order).
 
 
-const myArray = ["one",2,3,"four",5, "six",7,"eight",9,"ten"];
+const myArray = [["one",2,3,"four",5, "six",7,"eight",9,"ten"],[1,2,3,4,5,6,7,8,9,10]];
