94047809e53057e416c71de7d38287c3f3a98a91
IshniyazovFail <filimen@mail.ru>
2022-04-13 20:39:28 +0300

    Manipulate Arrays With shift()

--- Basic js/push.js
+++ Basic js/push.js
@@ -5,5 +5,10 @@ myArray.push(["dog", 3]);
 
 // Manipulate Arrays With pop()
 // Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
-const myArray = [["John", 23], ["cat", 2]];
+
 const removedFromMyArray = myArray.pop();
+
+// Manipulate Arrays With shift()
+// Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+const removedFromMyArray = myArray.shift();
