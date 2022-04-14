66394d25fa34280fcf2bd8e24af9d4b97ad570de
IshniyazovFail <filimen@mail.ru>
2022-04-14 10:52:43 +0300

    Passing Values to Functions with Arguments

---
+++ Basic js/functions.js
@@ -0,0 +1,17 @@
+/* Write Reusable JavaScript with Functions
+Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function. */
+
+function reusableFunction() {
+console.log("Hi World");
+}
+reusableFunction();
+
+/* Passing Values to Functions with Arguments
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments. */
+function functionWithArgs(a,b) {
+   console.log(a+b);
+}
+functionWithArgs(10,5);
+
