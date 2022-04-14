497ce0af5359c82e44dd9a5b58d3c828dcc24c8e
IshniyazovFail <filimen@mail.ru>
2022-04-14 11:34:57 +0300

    Assignment with a Returned Value

--- Basic js/functions.js
+++ Basic js/functions.js
@@ -15,3 +15,18 @@ function functionWithArgs(a,b) {
 }
 functionWithArgs(10,5);
 
+/* Return a Value from a Function with Return
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. */
+
+function timesFive(num){
+   return num *5;
+}
+
+// Assignment with a Returned Value
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
